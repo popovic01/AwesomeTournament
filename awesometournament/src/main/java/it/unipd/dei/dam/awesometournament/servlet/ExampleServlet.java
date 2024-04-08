0b0ea00e85aff7ab9204e5aa018d8a03770cce9d
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-04-08 21:21:33 +0200

    add logger and database context

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/servlet/ExampleServlet.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/servlet/ExampleServlet.java
@@ -1,20 +1,24 @@
-package it.unipd.dei.dam.awesometournament;
+package it.unipd.dei.dam.awesometournament.servlet;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.StringFormatterMessageFactory;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
 public class ExampleServlet extends HttpServlet {
+    protected final static Logger LOGGER = LogManager.getLogger(ExampleServlet.class, StringFormatterMessageFactory.INSTANCE);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.info("Received get request");
         req.setAttribute("group", "DAM");
         List<String> names = new ArrayList<String>();
         names.add("Alberto");
