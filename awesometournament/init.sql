efd1931f54a37b8d915ab37c8df55ccb5fce1bd9
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-04-17 11:53:43 +0200

    db population

--- awesometournament/init.sql
+++ awesometournament/init.sql
@@ -90,7 +90,7 @@ INSERT INTO public.tournaments(name, token,
 creator_user_id, max_teams, max_players,
 min_players, starting_players, max_substitutions,
 is_finished)
-VALUES ('seriea', 'token', 1, 20, 25, 15, 11,
+VALUES ('seriea', 'a1b2c3d4', 1, 20, 25, 15, 11,
 5, false);
 
 INSERT INTO public.teams(name, creator_user_id,
@@ -98,8 +98,9 @@ tournament_id)
 VALUES ('milan', 2, 1), ('inter', 3, 1);
 
 INSERT INTO public.matches(team1_id, team2_id,
-tournament_id, referee, is_finished)
-VALUES (1, 2, 1, 'nicola ferro', false);
+tournament_id, team1_score, team2_score, result, referee, match_date, is_finished)
+VALUES (1, 2, 1, 1, 3, '    team1', 'nicola ferro', '2024-04-17 18:00:00+03', true),
+(2, 1, 1, NULL, NULL, NULL, 'andrea stocco', '2024-04-20 18:00:00+03', false);
 
 INSERT INTO public.players(name, surname,
 team_id, date_of_birth)
