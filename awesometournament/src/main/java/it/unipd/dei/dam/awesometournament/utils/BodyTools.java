de0cdf2e4a391f2fb7060308b47579a18e379fd6
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-04-19 15:50:07 +0200

    code: fix parse of post body

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/utils/BodyTools.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/utils/BodyTools.java
@@ -1,25 +1,30 @@
 package it.unipd.dei.dam.awesometournament.utils;
 
 import java.io.BufferedReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BodyTools {
     public static Map<String, String> parsePostBody(BufferedReader reader) throws Exception{
-        Map<String, String> map = new HashMap<>();
         String line = reader.readLine();
-        if(line == null) {
-            throw new Exception("Malformed body");
-        }
-        String[] params = line.split("&");
-        for(String param: params) {
-            String[] parts = param.split("=");
-            if (parts.length != 2) {
-                throw new Exception("Malformed body");
+
+        Map<String, String> formData = new HashMap<>();
+        String[] pairs = line.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                try {
+                    String key = URLDecoder.decode(keyValue[0], "UTF-8");
+                    String value = URLDecoder.decode(keyValue[1], "UTF-8");
+                    formData.put(key, value);
+                } catch (UnsupportedEncodingException e) {
+                    throw new Exception("Malformed body");
+                }
             }
-            map.put(parts[0], parts[1]);
         }
-        return map;
+        return formData;
     }
     
 }
