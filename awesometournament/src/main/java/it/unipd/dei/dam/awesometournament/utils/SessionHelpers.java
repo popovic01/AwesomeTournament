5ebe01b8c6c7d1dde44bf7ab79d1287e3edb9b5b
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-04-21 15:21:28 +0200

    fix: revert SessionHelper

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/utils/SessionHelpers.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/utils/SessionHelpers.java
@@ -1,30 +1,25 @@
 package it.unipd.dei.dam.awesometournament.utils;
 
-import it.unipd.dei.dam.awesometournament.servlet.handler.SessionHandler;
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.message.StringFormatterMessageFactory;
+import jakarta.servlet.http.HttpSession;
 
 public class SessionHelpers {
     static class NotLoggedException extends RuntimeException{}
 
-    protected static final Logger LOGGER = LogManager.getLogger(SessionHandler.class,
-            StringFormatterMessageFactory.INSTANCE);
-
     public static boolean isLogged(HttpServletRequest req) {
-        LOGGER.info(req.getAttribute("session_id")); //set in SessionHandler
-        LOGGER.info(req.getAttribute("session_email")); //set in SessionHandler
-        if (req.getAttribute("session_id") != null) {
+        HttpSession session = req.getSession();
+        if(session != null && session.getAttribute("id") != null) {
             return true;
         }
         return false;
     }
 
     public static int getId(HttpServletRequest req) throws NotLoggedException {
-        if (!isLogged(req)) {
+        if(!isLogged(req)) {
             throw new NotLoggedException();
         }
-        return (int) req.getAttribute("session_id");
+
+        HttpSession session = req.getSession();
+        return (int) session.getAttribute("id");
     }
 }
