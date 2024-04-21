dea342d56aa576361e266e0638f6f85c2d63f507
Milica <milica.popovic@studenti.unipd.it>
2024-04-21 13:52:46 +0200

    refactor: improve UpdateTeamDAO

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/database/UpdateTeamDAO.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/database/UpdateTeamDAO.java
@@ -1,7 +1,6 @@
 package it.unipd.dei.dam.awesometournament.database;
 
 import it.unipd.dei.dam.awesometournament.resources.entities.Team;
-import jakarta.servlet.http.HttpServletResponse;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,16 +18,13 @@ public class UpdateTeamDAO extends AbstractDAO<Integer>  {
     @Override
     protected void doAccess() throws Exception {
         PreparedStatement p = null;
-
         try {
-            if (this.team.getLogo().isEmpty()) {
-                STATEMENT = "UPDATE public.teams SET name = ?, creator_user_id = ?, tournament_id = ? WHERE id = ?";
+            if (this.team.getLogo() == null) {
+                STATEMENT = "UPDATE public.teams SET name = ? WHERE id = ?";
                 p = con.prepareStatement(STATEMENT);
 
                 p.setString(1, this.team.getName());
-                p.setInt(2, this.team.getCreatorUserId());
-                p.setInt(3, this.team.getTournamentId());
-                p.setInt(4, this.team.getId());
+                p.setInt(2, this.team.getId());
             } else { //logo update
                 STATEMENT = "UPDATE public.teams SET logo = ? WHERE id = ?";
                 p = con.prepareStatement(STATEMENT);
