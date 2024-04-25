7c06322118cd9179a67506f06add82f1e546c452
AndreaBruzz <andrea.bruttomesso.1@studenti.unipd.it>
2024-04-25 10:13:39 +0200

    handle user session for match creation

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/servlet/CreateTournamentMatchesServlet.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/servlet/CreateTournamentMatchesServlet.java
@@ -1,14 +1,18 @@
 package it.unipd.dei.dam.awesometournament.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.StringFormatterMessageFactory;
 
+import it.unipd.dei.dam.awesometournament.database.GetTournamentByIdDAO;
 import it.unipd.dei.dam.awesometournament.jobs.MatchCreatorJob;
 import it.unipd.dei.dam.awesometournament.resources.Actions;
 import it.unipd.dei.dam.awesometournament.resources.LogContext;
+import it.unipd.dei.dam.awesometournament.resources.entities.Tournament;
+import it.unipd.dei.dam.awesometournament.utils.SessionHelpers;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,11 +23,11 @@ public class CreateTournamentMatchesServlet extends AbstractDatabaseServlet {
             StringFormatterMessageFactory.INSTANCE);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         LogContext.setIPAddress(req.getRemoteAddr());
         LogContext.setAction(Actions.PUT_MATCHES);
 
-        LOGGER.info("Received job request");
+        LOGGER.info("Received POST request");
         String url = req.getPathInfo();
         if (url != null) {
             String[] urlParts = url.split("/"); // urlParts[0] = ""
@@ -31,7 +35,29 @@ public class CreateTournamentMatchesServlet extends AbstractDatabaseServlet {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid URL format");
             } else {
 
+                if (!SessionHelpers.isLogged(req)) {
+                    LOGGER.info("user is not logged!");
+                    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                }
+
+                int loggedId = SessionHelpers.getId(req);
+                LOGGER.info("user id is " + loggedId);
+                
                 int tournamentId = Integer.parseInt(urlParts[1]);
+
+                try {
+                    GetTournamentByIdDAO getTournamentByIdDAO = new GetTournamentByIdDAO(getConnection(), tournamentId);
+                    Tournament tournament = getTournamentByIdDAO.access().getOutputParam();
+
+                    if (tournament.getCreatorUserId() != loggedId) {
+                        LOGGER.info("user has no access to this tournament!");
+                        resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                        throw new ServletException("user has no access to this tournament");
+                    }
+                } catch (SQLException e) {
+                    LOGGER.info(e);
+                }
+
                 try {
                     MatchCreatorJob.execute(tournamentId);
                     resp.getWriter().println("Match creation job has been executed successfully.");
