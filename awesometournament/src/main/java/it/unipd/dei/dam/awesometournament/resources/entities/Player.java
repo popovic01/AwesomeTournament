0b7cdb2940133312fa362257fd0031f0445402e6
Andrea Stocco <astocco11@gmail.com>
2024-04-28 20:51:11 +0200

    docs: add player and playerposition docs

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/resources/entities/Player.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/resources/entities/Player.java
@@ -9,36 +9,36 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import it.unipd.dei.dam.awesometournament.resources.enums.PlayerPosition;
 
 /**
- * A class which represents a player in a match.
+ * Represents a player in the database.
  */
 public class Player {
 
     /**
-     * The ID of the player.
+     * The unique identifier for the player.
      */
     @JsonProperty("id")
     private int id;
 
     /**
-     * The name of the player.
+     * The first name of the player.
      */
     @JsonProperty("name")
     private String name;
 
     /**
-     * The surname of the player.
+     * The last name of the player.
      */
     @JsonProperty("surname")
     private String surname;
 
     /**
-     * The ID of the team the player belongs to.
+     * The identifier of the team the player belongs to.
      */
     @JsonProperty("team_id")
     private int teamId;
 
     /**
-     * The position of the player in the team.
+     * The position of the player within the team.
      */
     @JsonProperty("position")
     private PlayerPosition position;
@@ -59,18 +59,18 @@ public class Player {
      * Default constructor for JSON deserialization.
      */
     @JsonCreator
-    public Player(){}
+    public Player() {}
 
     /**
-     * Constructs a player with the specified parameters.
+     * Constructs a new Player with the specified attributes.
      *
-     * @param id                 The ID of the player.
-     * @param name               The name of the player.
-     * @param surname            The surname of the player.
-     * @param teamId             The ID of the team the player belongs to.
-     * @param position           The position of the player in the team.
+     * @param id The unique identifier for the player.
+     * @param name The first name of the player.
+     * @param surname The last name of the player.
+     * @param teamId The identifier of the team the player belongs to.
+     * @param position The position of the player within the team.
      * @param medicalCertificate The medical certificate of the player.
-     * @param dateOfBirth        The date of birth of the player.
+     * @param dateOfBirth The date of birth of the player.
      */
     public Player(int id, String name, String surname, int teamId, PlayerPosition position, InputStream medicalCertificate, Date dateOfBirth) {
         this.id = id;
@@ -82,91 +82,93 @@ public class Player {
         this.dateOfBirth = dateOfBirth;
     }
 
+    // Getters and Setters
+
     /**
-     * Returns the ID of the player.
+     * Returns the unique identifier for the player.
      *
-     * @return The ID of the player.
+     * @return The unique identifier for the player.
      */
     public int getId() {
         return id;
     }
 
     /**
-     * Sets the ID of the player.
+     * Sets the unique identifier for the player.
      *
-     * @param id The ID of the player.
+     * @param id The unique identifier for the player.
      */
     public void setId(int id) {
         this.id = id;
     }
 
     /**
-     * Returns the name of the player.
+     * Returns the first name of the player.
      *
-     * @return The name of the player.
+     * @return The first name of the player.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Sets the name of the player.
+     * Sets the first name of the player.
      *
-     * @param name The name of the player.
+     * @param name The first name of the player.
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * Returns the surname of the player.
+     * Returns the last name of the player.
      *
-     * @return The surname of the player.
+     * @return The last name of the player.
      */
     public String getSurname() {
         return surname;
     }
 
     /**
-     * Sets the surname of the player.
+     * Sets the last name of the player.
      *
-     * @param surname The surname of the player.
+     * @param surname The last name of the player.
      */
     public void setSurname(String surname) {
         this.surname = surname;
     }
 
     /**
-     * Returns the ID of the team the player belongs to.
+     * Returns the identifier of the team the player belongs to.
      *
-     * @return The ID of the team the player belongs to.
+     * @return The identifier of the team the player belongs to.
      */
     public int getTeamId() {
         return teamId;
     }
 
     /**
-     * Sets the ID of the team the player belongs to.
+     * Sets the identifier of the team the player belongs to.
      *
-     * @param teamId The ID of the team the player belongs to.
+     * @param teamId The identifier of the team the player belongs to.
      */
     public void setTeamId(int teamId) {
         this.teamId = teamId;
     }
 
     /**
-     * Returns the position of the player in the team.
+     * Returns the position of the player within the team.
      *
-     * @return The position of the player in the team.
+     * @return The position of the player within the team.
      */
     public PlayerPosition getPosition() {
         return position;
     }
 
     /**
-     * Sets the position of the player in the team.
+     * Sets the position of the player within the team.
      *
-     * @param position The position of the player in the team.
+     * @param position The position of the player within the team.
      */
     public void setPosition(PlayerPosition position) {
         this.position = position;
@@ -209,9 +211,9 @@ public class Player {
     }
 
     /**
-     * Returns a string representation of the player.
+     * Returns a string representation of the Player object.
      *
-     * @return A string representation of the player.
+     * @return A string representation of the Player object.
      */
     @Override
     public String toString() {
