6aafe4e40ca091fb260a00065905f2dab95e87a6
Andrea Stocco <astocco11@gmail.com>
2024-04-28 21:15:52 +0200

    code: add setter and javadoc comments to ranking entry

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/utils/RankingEntry.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/utils/RankingEntry.java
@@ -1,32 +1,24 @@
 package it.unipd.dei.dam.awesometournament.utils;
 
 /**
- * Represents an entry in a ranking table.
+ * Represents an entry in a ranking, containing information about a team's name, points, and matches played.
  */
 public class RankingEntry {
-    /**
-     * The name of the team.
-     */
+    /** The name of the team. */
     String teamName;
-
-    /**
-     * The total points accumulated by the team.
-     */
+    /** The points accumulated by the team. */
     int points;
-
-    /**
-     * The total number of matches played by the team.
-     */
+    /** The number of matches played by the team. */
     int matchesPlayed;
 
     /**
-     * Constructs a ranking entry with the specified attributes.
+     * Constructs a new ranking entry with the specified team name, points, and matches played.
      *
-     * @param teamName      The name of the team.
-     * @param points        The total points accumulated by the team.
-     * @param matchesPlayed The total number of matches played by the team.
+     * @param teamName The name of the team.
+     * @param points The points accumulated by the team.
+     * @param matchesPlayed The number of matches played by the team.
      */
-    public RankingEntry (String teamName, int points, int matchesPlayed) {
+    public RankingEntry(String teamName, int points, int matchesPlayed) {
         this.teamName = teamName;
         this.points = points;
         this.matchesPlayed = matchesPlayed;
@@ -35,27 +27,54 @@ public class RankingEntry {
     /**
      * Gets the name of the team.
      *
-     * @return The name of the team.
+     * @return The team name.
      */
     public String getTeamName() {
         return teamName;
     }
 
     /**
-     * Gets the total points accumulated by the team.
+     * Gets the points accumulated by the team.
      *
-     * @return The total points accumulated by the team.
+     * @return The points.
      */
     public int getPoints() {
         return points;
     }
 
     /**
-     * Gets the total number of matches played by the team.
+     * Gets the number of matches played by the team.
      *
-     * @return The total number of matches played by the team.
+     * @return The number of matches played.
      */
-    public int getMatchesPlayed () {
+    public int getMatchesPlayed() {
         return matchesPlayed;
+    }
+
+    /**
+     * Sets the name of the team.
+     *
+     * @param teamName The name of the team.
+     */
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    /**
+     * Sets the points accumulated by the team.
+     *
+     * @param points The points to set.
+     */
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    /**
+     * Sets the number of matches played by the team.
+     *
+     * @param matchesPlayed The number of matches played.
+     */
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
     }
 }
