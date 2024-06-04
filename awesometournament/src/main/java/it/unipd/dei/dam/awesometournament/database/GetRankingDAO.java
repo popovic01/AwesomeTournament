37cb090c0203fa563d6581e3d67cfe77214755d4
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-06-04 16:37:46 +0200

    fix: fix query to show teams in ranking with no matches player

--- awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/database/GetRankingDAO.java
+++ awesometournament/src/main/java/it/unipd/dei/dam/awesometournament/database/GetRankingDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+
 import it.unipd.dei.dam.awesometournament.utils.RankingEntry;
 
 /**
@@ -17,10 +18,10 @@ public class GetRankingDAO extends AbstractDAO<ArrayList<RankingEntry>> {
                                             "SUM(CASE WHEN m.team1_id = t.id AND m.result = 'team1' THEN 3 " +  // Team 1 won
                                             "WHEN m.team2_id = t.id AND m.result = 'team2' THEN 3 " +  // Team 2 won
                                             "WHEN m.result = 'draw' THEN 1 " +                          // Draw
-                                            "ELSE 0 END) AS points, COUNT(*) AS matches_played " +
+                                            "WHEN m.is_finished = false THEN 0 ELSE 0 END) AS points, SUM(CASE WHEN m.is_finished = true THEN 1 ELSE 0 END) AS matches_played " +
                                             "FROM teams t " +
-                                            "JOIN matches m ON t.id = m.team1_id OR t.id = m.team2_id " +
-                                            "WHERE t.tournament_id = ? AND m.is_finished = true " +  // Only consider finished matches
+                                            "LEFT JOIN matches m ON t.id = m.team1_id OR t.id = m.team2_id " +
+                                            "WHERE t.tournament_id = ? " +  // Only consider finished matches
                                             "GROUP BY t.id, t.name, t.logo " +
                                             "ORDER BY points DESC, matches_played DESC;";
     /**
@@ -47,6 +48,7 @@ public class GetRankingDAO extends AbstractDAO<ArrayList<RankingEntry>> {
         try {
             p = con.prepareStatement(STATEMENT);
             p.setInt(1, this.tournamentId);
+            LOGGER.info(p);
             rs = p.executeQuery();
             while (rs.next()) {
                 LOGGER.info("Add team to ranking");
